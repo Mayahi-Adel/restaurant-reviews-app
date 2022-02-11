d1acd9b7e0a207de702db0ece41cfd389520b6cc
Mayahi Adel <adel.mayahii@gmail.com>
2022-02-11 23:48:04 +0100

    fix: add review page

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -75,7 +75,7 @@ const RestaurantsList = (props) => {
   };
 
   const findByCuisine = () => {
-    if (searchCuisine == "All Cuisines") {
+    if (searchCuisine === "All Cuisines") {
       refreshList();
     } else {
       find(searchCuisine, "cuisine");
